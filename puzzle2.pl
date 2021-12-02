625d0f9a8f7917519889b6c631483e865544ef39
Samuel Neaves <sn0550@IT017509.lan>
2021-12-02 10:12:57 +0000

    comment out

--- puzzle2.pl
+++ puzzle2.pl
@@ -36,11 +36,11 @@ test(X):-
 %%% Fun:
 %%% Find a plan to get to a position
 % ?- length(Plan,L),phrase(plan2(p(10,4,A)),Plan).
-Plan = [i(forward, _A), i(forward, _B), i(down, A)],
-L = 3,
-A*_A#=_C,
-A*_B#=_D,
-_B+_A#=10,
-_D+_C#=4 .
+% Plan = [i(forward, _A), i(forward, _B), i(down, A)],
+% L = 3,
+% A*_A#=_C,
+% A*_B#=_D,
+% _B+_A#=10,
+% _D+_C#=4 .
 
 %First plan found says the length has to be 3,  and constraints on the 3 input values. (inverse).
