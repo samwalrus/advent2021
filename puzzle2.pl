7c38f0d7f63c90431d32b794349fe4ff49fa4194
Samuel Neaves <sn0550@IT017509.lan>
2021-12-02 09:56:36 +0000

    part 2 puzzle 2

--- puzzle2.pl
+++ puzzle2.pl
@@ -11,6 +11,12 @@ plan(p(F1,D0)) --> [i(forward,Famount)],plan(p(F0,D0)),{ F1 #= F0 + Famount }.
 plan(p(F0,D1)) --> [i(down,Damount)],plan(p(F0,D0)),{ D1 #= D0 + Damount }.
 plan(p(F0,D1)) --> [i(up,Damount)],plan(p(F0,D0)),{ D1 #= D0 - Damount }.
 
+plan2(p(0,0,0)) --> [].
+plan2(p(F1,D1,Aim0)) --> [i(forward,Amount)],plan2(p(F0,D0,Aim0)),{ F1 #= F0 + Amount,D1#=D0+Aim0*Amount }.
+plan2(p(F0,D0,Aim)) --> [i(down,Amount)],plan2(p(F0,D0,Aim0)),{ Aim #= Aim0 + Amount }.
+plan2(p(F0,D0,Aim)) --> [i(up,Amount)],plan2(p(F0,D0,Aim0)),{ Aim #= Aim0 - Amount }.
+
+
 test(X):-
     X= [
     i(forward,5),
@@ -22,4 +28,7 @@ test(X):-
     ].
 
 %%% ?- test(X),phrase(plan(P),X).
+%%% ?- test(X), reverse(X,R),phrase(plan2(P),R).
+
 %%% csv_read_file('input2.csv', Rows, [functor(i),separator(0' )]),phrase(plan(p(F,D)),Rows),Total #=F*D.
+%%% csv_read_file('input2.csv', Rows, [functor(i),separator(0' )]), reverse(Rows,R),phrase(plan2(p(F,D,A)),R),Total #= F*D.
