46bf915e6918003fae1a8bc8cee0e1001063e213
Samuel Neaves <sn0550@IT017509.lan>
2021-12-03 06:47:20 +0000

    Dont conver to a number on read!

--- puzzle3.pl
+++ puzzle3.pl
@@ -1,6 +1,7 @@
 :-use_module(library(csv)).
 :-use_module(library(yall)).
 :-use_module(library(clpfd)).
+:-use_module(library(reif)).
 
 test(X):- X=['00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010'].
 
@@ -35,6 +36,18 @@ ns_index_max_min(Ns,I,MaxValue,MinValue):-
     maplist(nth1(I),Ns,Firsts),
     list_mcommon_lcommon(Firsts,MaxValue,MinValue).
 
+:- meta_predicate tcount(2,?,?).
+tcount(P_2,Xs,N) :-
+   N #>= 0,
+   list_pred_tcount_(Xs,P_2,0,N).
+
+:- meta_predicate list_pred_tcount_(?,2,?,?).
+list_pred_tcount_([]    , _ ,N ,N).
+list_pred_tcount_([X|Xs],P_2,N0,N) :-
+   if_(call(P_2,X), (N1 is N0+1, N1 #=< N), N1 = N0),
+   list_pred_tcount_(Xs,P_2,N1,N).
+
 
 %?- test(X),maplist(atom_codes,X,Codes),maplist(numbers_codes,Ns,Codes), Codes=[H|T],length(H,Len),numlist(1,Len,Numlist),maplist(ns_index_max_min(Ns),Numlist,Max,Min),binary_number(Max,Gamma),binary_number(Min,Ep),PC #= Gamma*Ep.
 
+%?- csv_read_file('input3.csv', Rows, [functor(i),separator(0' ),convert(false)]),maplist(i_atom,Rows,Atoms), maplist(atom_codes,Atoms,Codes),maplist(numbers_codes,Ns,Codes).
